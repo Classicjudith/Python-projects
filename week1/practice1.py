dd9918bbe3efad2bad12ebbd4ed9382bb3278e1f
Judith Okwu <ladyj@Judiths-MacBook-Pro.local>
2023-02-11 18:03:46 -0800

    FOLDER FOR WEEK ONE

---
+++ week1/practice1.py
@@ -0,0 +1,58 @@
+#numeric expression
+x = 1
+y = 3
+add = x + y
+print(add)
+
+x = 10
+y = 4
+sub = x - y
+print(sub)
+
+mult = x * y
+print(mult)
+
+#OPERATOR PRECEDENCE
+X = 1 + 2 - 10
+
+#Type
+x = int('23')
+
+x = 2 + 3
+print(x)
+
+
+
+#ASSIGNMENT.
+print ("hello, world")
+
+
+x = 4 
+y = x + 1
+x = 2 
+print(x,y) 
+
+
+x, y = 2, 6
+x, y = y, x + 2
+print (x, y)
+
+
+a, b = 2, 3
+c, b = a,
+c = 1 
+print (a, b, c)
+
+x = "hello" 
+y = 'world'
+print (x, y)
+
+x = 2 + 3
+print(x)
+
+
+
+
+
+
+
